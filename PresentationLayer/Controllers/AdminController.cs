83c3dd3525e67689f03498aff82ea2ae90cc3a87
ltphat2204 <ltphat2204@gmail.com>
2025-03-23 15:00:45 +0700

    Add crud action for groups

--- PresentationLayer/Controllers/AdminController.cs
+++ PresentationLayer/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
-﻿using BusinessLayer.Interfaces;
+﻿using BusinessLayer.Entities;
+using BusinessLayer.Interfaces;
+using DataAccessLayer;
 using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models.Group;
 
 namespace PresentationLayer.Controllers
 {
-    public class AdminController(IUnitOfWork unitOfWork, ILogger<AuthController> logger) : Controller
+    public class AdminController(IUnitOfWork _unitOfWork, ILogger<AuthController> logger) : Controller
     {
-
         public IActionResult Index()
         {
             return View();
@@ -14,8 +16,57 @@ namespace PresentationLayer.Controllers
         [HttpGet]
         public IActionResult Groups()
         {
-            var groups = unitOfWork.Groups.GetAll();
+            var groups = _unitOfWork.Groups.GetAll();
             return View(groups);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup(GroupCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var newGroup = new Group
+                {
+                    GroupName = model.GroupName,
+                };
+
+                _unitOfWork.Groups.Add(newGroup);
+                await _unitOfWork.SaveChangesAsync();
+
+                return RedirectToAction("Groups");
+            }
+
+            return View("Groups");
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditGroup(GroupEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var group = _unitOfWork.Groups.GetById(model.GroupId);
+                if (group != null)
+                {
+                    group.GroupName = model.GroupName;
+                    await _unitOfWork.SaveChangesAsync();
+                }
+
+                return RedirectToAction("Groups");
+            }
+
+            return View("Groups");
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteGroup(int groupId)
+        {
+            var group = _unitOfWork.Groups.GetById(groupId);
+            if (group != null)
+            {
+                _unitOfWork.Groups.Remove(group);
+                await _unitOfWork.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Groups");
+        }
+
     }
 }
---
+++ PresentationLayer/Models/Group/GroupCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresentationLayer.Models.Group
+{
+    public class GroupCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Group name")]
+        public required string GroupName { get; set; }
+    }
+}
