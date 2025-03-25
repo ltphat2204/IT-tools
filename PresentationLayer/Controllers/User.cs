ead922f941147e2d50ad0f9fe16c9c8186cb7e07
ponponl <hoianh2004@gmail.com>
2025-03-25 15:39:10 +0700

    Profile page first steps

--- PresentationLayer/Controllers/User.cs
+++ PresentationLayer/Controllers/User.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models.User;
+using DataAccessLayer;
 
 namespace PresentationLayer.Controllers
 {
-    public class User : Controller
+    public class UserController : Controller
     {
-        public IActionResult Profile()
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult Profile(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            UpdateProfileViewModel profile = new UpdateProfileViewModel
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+
+            return View(profile);
         }
     }
 }
--- PresentationLayer/Models/User/UpdateProfileViewModel.cs
+++ PresentationLayer/Models/User/UpdateProfileViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class UpdateProfileViewModel
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
     }
 }
