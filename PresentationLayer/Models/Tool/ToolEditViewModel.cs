0ce3c4f2789cdc3d7c85026f9f4d6d0dfb49248e
Lê Tấn Phát <ltphat2204@gmail.com>
2025-03-26 22:12:01 +0700

    Add hot plug tool

---
+++ PresentationLayer/Models/Tool/ToolEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresentationLayer.Models.Tool
+{
+    public class ToolEditViewModel
+    {
+        public int ToolId { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "UI is required.")]
+        public string? UIStringHtml { get; set; }
+
+        public string? Libraries { get; set; }
+
+        public string? Process { get; set; }
+
+        [Required(ErrorMessage = "Group is required.")]
+        public int GroupId { get; set; }
+
+        public IEnumerable<BusinessLayer.Entities.Group>? Groups { get; set; }
+    }
+}
